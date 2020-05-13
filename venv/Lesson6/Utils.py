a7b6bc595b776a29e79f63ed2f11922d651aec10
LE-S28 <a0963327198@gmail.com>
2020-05-13 19:57:43 +0800

    55

---
+++ venv/Lesson6/Utils.py
@@ -0,0 +1,17 @@
+import math
+import math as m
+from math import pi
+
+print(math.pi)
+print(m.pi)
+print(pi)
+
+import math
+import math as m
+form math import pi
+import lesson6.Utils as u
+form lessom6.utils import sub
+
+print(math.pi)
+pirnt(m.pi)
+print(pi)

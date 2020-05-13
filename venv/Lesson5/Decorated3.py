ab5b3e2fd29be12a3b3a7a44f69f6717fbe79d57
LE-S28 <a0963327198@gmail.com>
2020-05-13 19:21:04 +0800

    55

---
+++ venv/Lesson5/Decorated3.py
@@ -0,0 +1,22 @@
+# def make_dress(func):
+#     def dress():
+#         print("穿衣服")
+#         func()
+#     return dress
+# def out():
+#     print("我出門了")
+#
+#
+# m = make_dress(out)
+# m()
+
+def make_dress(func):
+    def dress():
+        print("穿衣服")
+        func()
+    return dress
+
+@make_dress
+def out():
+    print("我出門了")
+out()
